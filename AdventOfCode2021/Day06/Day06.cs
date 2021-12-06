efaa14fa6f46c887be682d607bebb15adff57647
Janis <no@mail.com>
2021-12-06 15:30:11 +0100

    Refactored Day 6

--- AdventOfCode2021/Day06/Day06.cs
+++ AdventOfCode2021/Day06/Day06.cs
@@ -8,61 +8,44 @@ internal class Day06
     public static void Task1and2()
     {
         List<int> fish = File.ReadAllLines(inputPath).First().Split(',').Select(Int32.Parse).ToList();
-        Dictionary<int, long> DayFish = new Dictionary<int, long>();
-        Dictionary<int, long> DayFish2 = new Dictionary<int, long>();
+        long[] dayFish = new long[9];
+        long[] dayFish2 = new long[9];
         int daysTask1 = 80;
         int daysTask2 = 256;
 
         foreach (int f in fish)
         {
-            if(!DayFish.TryAdd(f, 1))
-            {
-                DayFish[f]++;
-                
-            }
-            if (!DayFish2.TryAdd(f, 1))
-            {
-                DayFish2[f]++;
-            }
+            dayFish[f]++;
+            dayFish2[f]++;
         }
         
-        DayFish = SimulateDays(DayFish, daysTask1);
-        DayFish2 = SimulateDays(DayFish2, daysTask2);
+        dayFish = SimulateDays(dayFish, daysTask1);
+        dayFish2 = SimulateDays(dayFish2, daysTask2);
 
-        Console.WriteLine($"Task 1: {DayFish.Sum(n => n.Value)}");
-        Console.WriteLine($"Task 2: {DayFish2.Sum(n => n.Value)}");
+        Console.WriteLine($"Task 1: {dayFish.Sum()}");
+        Console.WriteLine($"Task 2: {dayFish2.Sum()}");
     }
 
-    private static Dictionary<int, long> SimulateDays(Dictionary<int, long> DayFish, int days)
+    private static long[] SimulateDays(long[] dayFish, int days)
     {
-        for (int i = 0; i < days; i++)
+        for (int day = 0; day < days; day++)
         {
-            Dictionary<int, long> newFish = new Dictionary<int, long>();
-            foreach (KeyValuePair<int, long> pair in DayFish)
+            long[] newFish = new long[9];
+            for (int i = 0; i < dayFish.Length; i++)
             {
-                if (pair.Key == 0)
+                if (i == 0)
                 {
-                    if (!newFish.TryAdd(8, pair.Value))
-                    {
-                        newFish[8] += pair.Value;
-                    }
-                    if (!newFish.TryAdd(6, pair.Value))
-                    {
-                        newFish[6] += pair.Value;
-                    }
+                    newFish[8] = dayFish[i];
+                    newFish[6] = dayFish[i];
                 }
                 else
                 {
-                    if (!newFish.TryAdd(pair.Key - 1, pair.Value))
-                    {
-                        newFish[pair.Key - 1] += pair.Value;
-                    }
+                    newFish[i - 1] += dayFish[i];
                 }
             }
-            DayFish = newFish;
+            dayFish = newFish;
         }
 
-        return DayFish;
+        return dayFish;
     }
 }
-
