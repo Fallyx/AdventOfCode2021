2c670070e967d09c6339a3ebe95a8b982f2f9544
Janis <no@mail.com>
2021-12-14 13:22:29 +0100

    Refactored Day 14

--- AdventOfCode2021/Day14/Day14.cs
+++ AdventOfCode2021/Day14/Day14.cs
@@ -6,58 +6,46 @@ internal class Day14
 
     public static void Task1()
     {
-        List<string> lines = File.ReadAllLines(inputPath).ToList();
-        string polymerTemplate = lines[0];
-        Dictionary<string, string> polymerReaction = new Dictionary<string, string>();
-        int steps = 10;
-
-        for(int i = 2; i < lines.Count; i++)
-        {
-            string[] pairInsertion = lines[i].Split(" -> ");
-            polymerReaction.Add(pairInsertion[0], pairInsertion[1]);
-        }
-
-        for (int i = 0; i < steps; i++)
-        {
-            for (int x = polymerTemplate.Length - 1; x >= 1; x--)
-            {
-                polymerTemplate = polymerTemplate.Insert(x, polymerReaction[polymerTemplate.Substring(x - 1, 2)]);
-            }
-        }
-
-        char[] chars = polymerTemplate.Distinct().ToArray();
-
-        int minChars = int.MaxValue;
-        int maxChars = int.MinValue;
-
-        foreach(char c in chars)
-        {
-            int count = polymerTemplate.Count(s => s == c);
-            if (count < minChars) minChars = count;
-            if (count > maxChars) maxChars = count;
-        }
-
-        Console.WriteLine($"Task 1: {maxChars - minChars}");
+        ProcessPolymer(1, 10);
     }
 
     public static void Task2()
     {
+        ProcessPolymer(2, 40);
+    }
+
+    private static void ProcessPolymer(int taskNum, int steps)
+    {
         List<string> lines = File.ReadAllLines(inputPath).ToList();
-        Dictionary<string, long> polymerPairs = new Dictionary<string, long>();
-        Dictionary<string, string> polymerReaction = new Dictionary<string, string>();
-        int steps = 40;
+        HashSet<char> chars = new HashSet<char>();
+        Dictionary<string, string> polymerReactions = GeneratePolymerReactions(lines.Skip(2).ToList(), chars);
+        Dictionary<string, long> polymerPairs = GeneratePolymerPairs(polymerReactions, lines[0], steps);
+        Console.WriteLine($"Task {taskNum}: {CalculateResult(polymerPairs, lines[0][0], chars)}");
+    }
 
-        for (int i = 0; i < lines[0].Length - 1; i++)
-        {
-            string key = lines[0][i] + "" + lines[0][i + 1];
-            if (!polymerPairs.TryAdd(key, 1))
-                polymerPairs[key]++;
-        }
-
-        for (int i = 2; i < lines.Count; i++)
+    private static Dictionary<string, string> GeneratePolymerReactions(List<string> lines, HashSet<char> chars)
+    {
+        Dictionary<string, string> polymerReactions = new Dictionary<string, string>();
+        for (int i = 0; i < lines.Count; i++)
         {
             string[] pairInsertion = lines[i].Split(" -> ");
-            polymerReaction.Add(pairInsertion[0], pairInsertion[1]);
+            polymerReactions.Add(pairInsertion[0], pairInsertion[1]);
+            chars.Add(pairInsertion[0][0]);
+            chars.Add(pairInsertion[0][1]);
+            chars.Add(pairInsertion[1][0]);
+        }
+
+        return polymerReactions;
+    }
+
+    private static Dictionary<string, long> GeneratePolymerPairs(Dictionary<string, string> polymerReactions, string template, int steps)
+    {
+        Dictionary<string, long> polymerPairs = new Dictionary<string, long>();
+        for (int i = 0; i < template.Length - 1; i++)
+        {
+            string key = $"{template[i]}{template[i + 1]}";
+            if (!polymerPairs.TryAdd(key, 1))
+                polymerPairs[key]++;
         }
 
         for (int i = 0; i < steps; i++)
@@ -66,27 +54,24 @@ internal class Day14
             foreach (string key in polymerPairs.Keys)
             {
                 long amount = polymerPairs[key];
-                string key1 = key[0] + polymerReaction[key];
-                string key2 = polymerReaction[key] + key[1];
+                string key1 = key[0] + polymerReactions[key];
+                string key2 = polymerReactions[key] + key[1];
                 if (!temp.TryAdd(key1, amount))
-                    temp[key1]+= amount;
+                    temp[key1] += amount;
                 if (!temp.TryAdd(key2, amount))
-                    temp[key2]+=amount;
+                    temp[key2] += amount;
             }
 
             polymerPairs = temp;
         }
 
+        return polymerPairs;
+    }
+
+    private static long CalculateResult(Dictionary<string, long> polymerPairs, char firstChar, HashSet<char> chars)
+    {
         long minChars = long.MaxValue;
         long maxChars = long.MinValue;
-
-        HashSet<char> chars = new HashSet<char>();
-
-        foreach (string key in polymerPairs.Keys)
-        {
-            chars.Add(key[0]);
-            chars.Add(key[1]);
-        }
 
         foreach (char c in chars)
         {
@@ -99,12 +84,11 @@ internal class Day14
                     count += polymerPairs[key];
             }
 
-            if (lines[0][0] == c) count++;
-
+            if (firstChar == c) count++;
             if (count < minChars) minChars = count;
             if (count > maxChars) maxChars = count;
         }
 
-        Console.WriteLine($"Task 2: {maxChars - minChars}");
+        return maxChars - minChars;
     }
 }
